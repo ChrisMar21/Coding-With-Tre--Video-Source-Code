45c5679c68ab2744eb3648ce25da905ec3424865
Chris Martin <chrismartin16cm@gmail.com>
2021-01-25 20:48:29 -0500

    Add files via upload

---
+++ Operators in Java (YouTube)/src/Operators.java
@@ -0,0 +1,12 @@
+public class Operators{
+    public static void main(String[] args) {
+        //instanceof Operator
+        String name = "Steve Austin";
+        if(name instanceof String){
+            System.out.println("True.");
+        }
+        else{
+            System.out.println("False.");
+        }
+    }
+}
